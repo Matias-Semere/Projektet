114cd4338a1c346967df80741dd84fd618f87722
desuberihu22-dotcom <desuberihu22@gmail.com>
2025-10-08 17:04:33 +0200

    ttt

--- Projektet/src/App.java
+++ Projektet/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Helloooooo sdjfsodifjsoådifj");
+        double a = 3.14;
+        System.out.println(a);              
     }
 }
