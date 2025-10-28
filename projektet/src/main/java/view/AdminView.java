d166232a8b6121311f4cb2a4701d27acd6cb54ae
Matias Semere <MatiasSolomon21@gmail.com>
2025-10-28 17:22:27 +0100

    fix adminview

--- projektet/src/main/java/view/AdminView.java
+++ projektet/src/main/java/view/AdminView.java
@@ -1,19 +1,11 @@
 package view;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.*;
 import java.util.Map;
 
-import javax.swing.DefaultListModel;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 import controller.AdminCon;
-import controller.MainController;
 import model.Betyg;
 import model.Student;
 import model.Betyg.Grades;
@@ -21,43 +13,48 @@ import model.Betyg.Grades;
 public class AdminView extends JPanel {
     // Betyg betyg;
 
-    // private MainController mc = new MainController();
-    // private AdminCon ac;
-    // private JList<Student> studentList;
-    // private DefaultListModel<String> listModel;
-    // private JComboBox<Enum> selectGrade;
-    // private JLabel valdStudentLabel;
-    // private Map<String, String> studentBetyg;
-    // private JTextField studentID = new JTextField();
-    // private String htmlstyle = "<html><style> h3 {color: White; border: 10px solid black; padding: 10px;}</style> <h3>";
+    private AdminCon ac;
+    private DefaultListModel<Student> listModel = new DefaultListModel<>();
+    private JList<Student> studentList = new JList<Student>(listModel);
+    private JScrollPane scrollPane = new JScrollPane(studentList);
+    private JComboBox<Enum> selectGrade;
+    private JLabel valdStudentLabel;
+    private JButton addButton;
+    private Map<String, String> studentBetyg;
+    private JTextField studentID = new JTextField();
+    private String htmlstyle = "<html><style> h3 {color: White; border: 10px solid black; padding: 10px;}</style> <h3>";
 
     public AdminView(AdminCon adminCon) {
-        // this.ac = adminCon;
-        // Betyg.Grades grades[] = { Betyg.Grades.A, Betyg.Grades.B, Betyg.Grades.C,
-        //         Betyg.Grades.D, Betyg.Grades.E, Betyg.Grades.F,
-        //         Betyg.Grades.MVG, Betyg.Grades.VG, Betyg.Grades.G,
-        //         Betyg.Grades.U };
+        this.ac = adminCon;
+        initComponents();
+        studentList.addListSelectionListener(e -> {
+            Object sel = selectGrade.getSelectedItem();
+            Grades grade = (Grades) sel;
+            Betyg betyg = new Betyg(12, 13, grade);
+            System.out.println(betyg);
+        });
 
-        // String[] b = { "A", "B", "C", "D", "E", "F" };
-        // selectGrade = new JComboBox<>(grades);
-
-        // listModel = new DefaultListModel<>();
-
-        // studentList.addListSelectionListener(e -> {
-        //     // betyg.setGrade(selectGrade.getSelectedItem());
-        //     Object sel = selectGrade.getSelectedItem();
-        //     Grades grade = (Grades) sel;
-        //     betyg = new Betyg(12, 13, grade);
-
-        // });
-
-        // add(selectGrade);
-        // add(studentID);
-        // add(studentList);
+        add(selectGrade);
+        add(studentID);
+        add(studentList);
 
         setSize(500, 500);
         setBackground(Color.DARK_GRAY);
 
+    }
+
+    public void initComponents() {
+        Betyg.Grades grades[] = { Betyg.Grades.A, Betyg.Grades.B, Betyg.Grades.C,
+                Betyg.Grades.D, Betyg.Grades.E, Betyg.Grades.F,
+                Betyg.Grades.MVG, Betyg.Grades.VG, Betyg.Grades.G,
+                Betyg.Grades.U };
+
+        String[] b = { "A", "B", "C", "D", "E", "F" };
+        selectGrade = new JComboBox<>(grades);
+
+
+        addButton = new JButton("Lägg till Student");
+        add(addButton);
     }
 
 }
