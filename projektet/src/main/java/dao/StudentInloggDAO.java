3843007dcb4d9a77e6648730f9e533121e1f7d29
e <hedstromfredrik10@gmail.com>
2025-10-28 12:10:20 +0100

    AlterStudentinlogg

--- projektet/src/main/java/dao/StudentInloggDAO.java
+++ projektet/src/main/java/dao/StudentInloggDAO.java
@@ -31,4 +31,17 @@ public class StudentInloggDAO {
         }
     }
 
+    public void alterStudentInlogg(StudentInlogg studentInlogg) {
+        String sql = "UPDATE StudentInloggning SET StudentID = ?, Lösenord = ?, WHERE StudentID = ?";
+        try (PreparedStatement ps = DataBase.getConnection().prepareStatement(sql)) {
+            ps.setInt(1, studentInlogg.getStudentID());
+            ps.setString(2, studentInlogg.getlösenord());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+
 }
