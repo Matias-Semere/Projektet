9713fbfd0c372118a388c5eb3970634940cedd5a
e <hedstromfredrik10@gmail.com>
2025-10-28 12:06:23 +0100

    StudentInloggDAO 2

--- projektet/src/main/java/dao/StudentInloggDAO.java
+++ projektet/src/main/java/dao/StudentInloggDAO.java
@@ -1,5 +1,34 @@
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.StudentInlogg;
+
 public class StudentInloggDAO {
-    
+
+    public void insertStudentInlogg(StudentInlogg studentInlogg) {
+        String sql = "INSERT INTO StudentInloggning VALUES (?, ?)";
+
+        try (PreparedStatement ps = DataBase.getConnection().prepareStatement(sql)) {
+            ps.setInt(1, studentInlogg.getStudentID());
+            ps.setString(2, studentInlogg.getlösenord());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteStudentInlogg(StudentInlogg studentInlogg) {
+        String sql = "DELETE FROM StudentInloggning WHERE StudentID = ?";
+
+        try (PreparedStatement ps = DataBase.getConnection().prepareStatement(sql)) {
+            ps.setInt(1, studentInlogg.getStudentID());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
